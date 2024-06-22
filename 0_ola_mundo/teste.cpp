32dedd889fadaea4031a6cb0aeea8470cc634973
julio-fm <julio.favero@sp.senai.br>
2024-06-22 09:19:31 -0300

    project start

---
+++ 0_ola_mundo/teste.cpp
@@ -0,0 +1,17 @@
+
+/*
+Autor: Julio Miranda
+Data: 20/06/2024
+Descrição: Apresenta a mensagem de Boas-vindas
+            aos novos desenvoveldores
+*/
+
+#include <iostream>
+
+using namespace std; //sem a necissidade de inserir "std::" exemplo: std::cout
+
+int main(){
+
+    cout<<"Hello World!!"<<endl;
+    return 0;
+}
